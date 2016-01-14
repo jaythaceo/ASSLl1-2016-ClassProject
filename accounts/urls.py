d240be511a35c318b4fc220e119a5894e4d6d4dc
Sonoflight <coreygumbs@gmail.com>
2016-01-14 16:02:30 -0500

    updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from views import (index, newUser, accountUpdate)
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^registration/$', newUser, name='registration' ),
-    url(r'^account-update/$', accountUpdate, name='update')
+    url(r'^account-update/$', accountUpdate, name='update'),
 ] 
